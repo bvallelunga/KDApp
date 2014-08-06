fa459796ee0a335ce4329a3622d5b6897c74b1c6
Brian Vallelunga <vallelungabrian@gmail.com>
2014-08-06 20:27:45 +0000

    added protection to scripts without arguments

--- apps/InstallerSkeleton.kdapp/scripts/install.sh
+++ apps/InstallerSkeleton.kdapp/scripts/install.sh
@@ -1,9 +1,22 @@
 #       Setup - DO NOT TOUCH        #
 # --------------------------------- #
+if [ $# -lt 2 ]
+  then
+  echo "Please provide a username and log output"
+  echo "bash install.sh <user> <log output>"
+  echo ""
+  exit 0
+else
+  USER=$1
+  OUT=$2
+  rm -rf $OUT/*
+  mkdir -p $OUT
+fi
+
 USER=$1
 OUT=$2
-rm -rf $OUT/*
-mkdir -p $OUT
+#rm -rf $OUT/*
+#mkdir -p $OUT
 
 #       Start Coding Here...        #
 # --------------------------------- #
--- apps/InstallerSkeleton.kdapp/scripts/reinstall.sh
+++ apps/InstallerSkeleton.kdapp/scripts/reinstall.sh
@@ -1,9 +1,17 @@
 #       Setup - DO NOT TOUCH        #
 # --------------------------------- #
-USER=$1
-OUT=$2
-rm -rf $OUT/*
-mkdir -p $OUT
+if [ $# -lt 2 ]
+  then
+  echo "Please provide a username and log output"
+  echo "bash reinstall.sh <user> <log output>"
+  echo ""
+  exit 0
+else
+  USER=$1
+  OUT=$2
+  rm -rf $OUT/*
+  mkdir -p $OUT
+fi
 
 #       Start Coding Here...        #
 # --------------------------------- #
